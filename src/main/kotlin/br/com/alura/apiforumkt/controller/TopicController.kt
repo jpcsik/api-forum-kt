8be4f59621db5cd8eb83219580b4735289de138a
jpcsik <joao.batista@zup.com.br>
2021-09-06 17:09:35 -0300

    response entity

--- src/main/kotlin/br/com/alura/apiforumkt/controller/TopicController.kt
+++ src/main/kotlin/br/com/alura/apiforumkt/controller/TopicController.kt
@@ -3,35 +3,41 @@ package br.com.alura.apiforumkt.controller
 import br.com.alura.apiforumkt.dto.NewTopicRequest
 import br.com.alura.apiforumkt.dto.UpdateTopicRequest
 import br.com.alura.apiforumkt.service.TopicService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.net.URI
 import javax.validation.Valid
+import kotlin.random.Random
 
 @RestController
 @RequestMapping("/topics")
 class TopicController(private val service: TopicService) {
 
     @GetMapping
-    fun list(): List<String> {
-        return service.list()
+    fun list(): ResponseEntity<Any> {
+        return ResponseEntity.ok(service.list())
     }
 
     @GetMapping("/{id}")
-    fun findById(@PathVariable id: Long): String {
-        return service.findById(id)
+    fun findById(@PathVariable id: Long): ResponseEntity<Any> {
+        return ResponseEntity.ok(service.findById(id))
     }
 
     @PostMapping
-    fun create(@RequestBody @Valid topicDto: NewTopicRequest) {
+    fun create(@RequestBody @Valid topicDto: NewTopicRequest): ResponseEntity<Any> {
         service.create(topicDto)
+        return ResponseEntity.created(URI.create("/topic/${Random.nextInt()}")).build()
     }
 
     @PutMapping("/{id}")
-    fun update(@PathVariable id: Long, @RequestBody @Valid topicDto: UpdateTopicRequest){
-        service.update(id, topicDto)
+    fun update(@PathVariable id: Long, @RequestBody @Valid topicDto: UpdateTopicRequest): ResponseEntity<Any>{
+        return ResponseEntity.ok(service.update(id, topicDto))
     }
 
     @DeleteMapping("/{id}")
-    fun delete(@PathVariable id: Long){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Long) {
         service.delete(id)
     }
 
